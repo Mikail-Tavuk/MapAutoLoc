495e6f493b666b3af42192fa19e7d2226082ab8c
Mikail Tavukcuoglu <mika.tavuk@gmail.com>
2020-10-22 17:45:40 +0200

    creation de l'appli react

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MApView, Permissions } from 'expo';
+import MapView from 'react-native-maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <MapView style={styles.map}>
+
+      </MapView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +21,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  map: {
+    flex: 1
+  }
 });
