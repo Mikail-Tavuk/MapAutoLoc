681ad1da411391b3ef9f097e8b6ca3956f73ffa0
Mikail Tavukcuoglu <mika.tavuk@gmail.com>
2020-10-22 19:10:56 +0200

    tentative d'ajout de la geolocalisation qui ne fonctionne pas

--- App.js
+++ App.js
@@ -1,16 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { MApView, Permissions } from 'expo';
-import MapView from 'react-native-maps';
+import { MapView, Permissions } from 'expo-permissions';
+// import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <MapView style={styles.map}>
+  state = {
+    latitude: null,
+    longitude: null
+  }
 
-      </MapView>
-    );
+  async componentDidMount() {
+    const { status } = await  Permissions.getAsync(Permissions.LOCATION)
+  
+
+    if (status != 'granted' ) {
+      const response = await Permissions.askAsync(Permissions.LOCATION)
+    }
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude }}) => this.setState({ latitude, longitude}, () => console.log('State' , this.state)),
+      (error) => console.log('Error', error)
+    )
+  }
+
+  render() {
+    const { latitude, longitude } = this.state
+    if (latitude) {
+      return (
+        <MapView style={styles.map}
+          initialRegion={{
+            latitude,
+            longitude,
+            latitudeDelta:  0.0922,
+            longitudeDelta: 0.0421
+          }}
+        >
+        </MapView>
+      );
+    }
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Make shure you enable your location to use this application</Text>
+      </View>
+    )
   }
 }
 
@@ -26,3 +58,34 @@ const styles = StyleSheet.create({
     flex: 1
   }
 });
+
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { MApView, Permissions } from 'expo';
+// import MapView from 'react-native-maps';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <MapView style={styles.map}>
+
+//       </MapView>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+
+//   map: {
+//     flex: 1
+//   }
+// });
